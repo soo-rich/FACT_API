072bdace8b5705e25da9f8cc2d886020a721298c
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-01-11 15:10:54 +0000

    use own exception on auth

--- src/main/java/com/soosmart/facts/dto/user/authentication/AuthenticationDTO.java
+++ src/main/java/com/soosmart/facts/dto/user/authentication/AuthenticationDTO.java
@@ -1,15 +1,17 @@
 package com.soosmart.facts.dto.user.authentication;
 
+import com.soosmart.facts.exceptions.dto.DtoArgumentRquired;
+
 public record AuthenticationDTO(
         String username,
         String password
 ) {
     public AuthenticationDTO {
         if (username == null || username.isBlank()) {
-            throw new IllegalArgumentException("Username cannot be null or empty");
+            throw new DtoArgumentRquired("Username cannot be null or empty");
         }
         if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException("Password cannot be null or empty");
+            throw new DtoArgumentRquired("Password cannot be null or empty");
         }
     }
 
--- src/main/java/com/soosmart/facts/dto/user/authentication/RefreshTokenDTO.java
+++ src/main/java/com/soosmart/facts/dto/user/authentication/RefreshTokenDTO.java
@@ -1,6 +1,13 @@
 package com.soosmart.facts.dto.user.authentication;
 
+import com.soosmart.facts.exceptions.dto.DtoArgumentRquired;
+
 public record RefreshTokenDTO(
         String refresh
 ) {
+    public RefreshTokenDTO{
+        if (refresh == null|| refresh.isBlank()) {
+         throw new DtoArgumentRquired("refresh token is blank or null");
+        }
+    }
 }
