fe391ad83ebab6b3b4b7965f63552e7a0d5f24e3
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-02-16 18:25:53 +0000

    Add CommandLineRunner to create a super admin on application startup

--- src/main/java/com/soosmart/facts/FactsApplication.java
+++ src/main/java/com/soosmart/facts/FactsApplication.java
@@ -1,15 +1,33 @@
 package com.soosmart.facts;
 
+import com.soosmart.facts.service.UtilisateurService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class FactsApplication {
+@AllArgsConstructor
+public class FactsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FactsApplication.class, args);
-	}
+    private UtilisateurService utilisateurService;
 
+    public static void main(String[] args) {
+        SpringApplication.run(FactsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            utilisateurService.createSuprerAdmin(
+                    "admin@admin.com",
+                    "admin",
+                    "admin"
+            );
+        } catch (Exception e) {
+            System.out.println("Super admin already exists");
+        }
+    }
 }
