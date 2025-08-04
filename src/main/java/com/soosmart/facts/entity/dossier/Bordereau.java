9f25bdb7fca63fe25388156316b79560eebb5661
Sul <sessoklinaulrich@gmail.com>
2025-08-04 19:10:35 +0000

    feat: add paied field to DocumentDTO and update ReportImpl to initialize isPaid status

--- src/main/java/com/soosmart/facts/entity/dossier/Bordereau.java
+++ src/main/java/com/soosmart/facts/entity/dossier/Bordereau.java
@@ -3,7 +3,6 @@ package com.soosmart.facts.entity.dossier;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,9 +18,6 @@ public class Bordereau extends Document {
 
     @OneToOne
     Proforma proforma;
-
-    @Builder.Default
-    private Boolean adopted = false;
 
     @Override
     public String toString() {
