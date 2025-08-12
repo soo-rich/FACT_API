1f453492f36a1208a15a7d72dee00a036419cc9a
Sul <sessoklinaulrich@gmail.com>
2025-08-12 13:28:12 +0000

    feat: update application properties for dynamic mail configuration and add scheduled health checks

---
+++ src/main/java/com/soosmart/facts/utils/CheckJobCron.java
@@ -0,0 +1,47 @@
+package com.soosmart.facts.utils;
+
+import com.soosmart.facts.dto.pagination.PaginatedRequest;
+import com.soosmart.facts.repository.ArticleDAO;
+import com.soosmart.facts.utils.pagination.PageMapperUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class CheckJobCron {
+    private static final Logger logger = LoggerFactory.getLogger(CheckJobCron.class);
+
+    private final ArticleDAO articleDAO;
+    private RestTemplate restTemplate;
+
+    public CheckJobCron(ArticleDAO articleDAO) {
+        this.articleDAO = articleDAO;
+    }
+
+    @Scheduled(fixedRate = 20000) // Runs every 20 seconds
+    public void checkdatabaseNeon() {
+        logger.info("Checking database Neon...");
+        try {
+            articleDAO.findAllBySupprimerIsFalse(PageMapperUtils.createPageableWithoutSearch(new PaginatedRequest(1, 1, null)));
+            logger.info("Database Neon check completed successfully.");
+        } catch (Exception e) {
+            logger.error("Error checking database Neon: {}", e.getMessage(), e);
+        }
+    }
+
+    @Scheduled(fixedRate = 20000) // Runs every 20 seconds
+    public void checkApplicationhealthy() {
+        logger.info("Checking application health...");
+        try {
+            String url = "https://soosmart-facts-api-latest.onrender.com/api/actuator/health"; // Replace with your actual health check URL
+            logger.info("Application is healthy.");
+            restTemplate.getForObject(url, Map.class);
+        } catch (Exception e) {
+            logger.error("Error during application health check: {}", e.getMessage(), e);
+        }
+    }
+}
