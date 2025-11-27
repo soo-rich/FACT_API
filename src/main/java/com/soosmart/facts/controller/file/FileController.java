773c7267e7facdfd7905c463751243e6534b4f2f
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-11-27 16:33:09 +0000

    refactor: comment out unused imports and methods in FileController

--- src/main/java/com/soosmart/facts/controller/file/FileController.java
+++ src/main/java/com/soosmart/facts/controller/file/FileController.java
@@ -1,34 +1,34 @@
 package com.soosmart.facts.controller.file;
 
-import com.soosmart.facts.service.file.FileMetadataService;
+// import com.soosmart.facts.service.file.FileMetadataService;
 import com.soosmart.facts.service.file.FileStorageService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
+// import java.util.UUID;
 
-import static com.soosmart.facts.utils.FileUtlis.generateUniqueFileName;
-import static com.soosmart.facts.utils.FileUtlis.getFileExtension;
+// import static com.soosmart.facts.utils.FileUtlis.generateUniqueFileName;
+// import static com.soosmart.facts.utils.FileUtlis.getFileExtension;
 
 @RestController()
 @RequestMapping("file")
 @AllArgsConstructor
 public class FileController {
-    private FileMetadataService fileMetadataService;
+    // private FileMetadataService fileMetadataService;
     private FileStorageService fileStorageService;
 
 
-    @PostMapping(value = "uploader-file", consumes = "multipart/form-data")
-    public ResponseEntity<?> uploadFile(@RequestPart("file") MultipartFile file) {
-//        return ResponseEntity.status(HttpStatus.OK).body(this.fileMetadataService.save(file,"test", "test"));
-        return ResponseEntity.status(HttpStatus.OK).body(this.fileStorageService.uploadFileToSubFolder(file,
-                String.format("/%s/%s", "test", generateUniqueFileName("test--"+ UUID.randomUUID().toString(),getFileExtension(file.getOriginalFilename())))));
-    }
+//     @PostMapping(value = "uploader-file", consumes = "multipart/form-data")
+//     public ResponseEntity<?> uploadFile(@RequestPart("file") MultipartFile file) {
+// //        return ResponseEntity.status(HttpStatus.OK).body(this.fileMetadataService.save(file,"test", "test"));
+//         return ResponseEntity.status(HttpStatus.OK).body(this.fileStorageService.uploadFileToSubFolder(file,
+//                 String.format("/%s/%s", "test", generateUniqueFileName("test--"+ UUID.randomUUID().toString(),getFileExtension(file.getOriginalFilename())))));
+//     }
 
     @GetMapping(path = "presigned")
     public ResponseEntity<Map<String, String>> pResponseEntity(@RequestParam("url") String url) {
