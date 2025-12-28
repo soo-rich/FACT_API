1a385390d9a95e110d512a4c60f11c6b9dd66802
SOO Ulrich <sessoklinaulrich@gmail.com>
2025-12-28 00:04:28 +0000

    refactor: streamline number generation methods for proforma, facture, and bordereau

--- src/main/java/com/soosmart/facts/utils/NumeroGenerateur.java
+++ src/main/java/com/soosmart/facts/utils/NumeroGenerateur.java
@@ -16,30 +16,45 @@ public class NumeroGenerateur {
     private final ProformaDao proformaRepository;
     private final FactureDao factureRepository;
     private final BorderauDao borderauRepository;
+
     // Générer un numéro de proforma
     public String GenerateproformaNumero() {
         Instant startOfDay = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant(); // Début de la journée
-        Instant endOfDay = LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant(); // Fin de la journée
-        return "FP" + this.ContatenationDesChiffreduJour()+ this.proformaRepository.countProformasCreateToday(startOfDay, endOfDay);
+        Instant endOfDay = LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant(); // Fin de la
+                                                                                                         // journée
+
+        Long numbreoftoday = this.proformaRepository.countProformasCreateToday(startOfDay, endOfDay);
+
+        return "FP" + this.ContatenationDesChiffreduJour()
+                + (numbreoftoday <= 9 ? "0" + numbreoftoday : numbreoftoday);
     }
 
     // Générer un numéro de facture
     public String GenerateFactureNumero() {
         Instant startOfDay = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant(); // Début de la journée
-        Instant endOfDay = LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant(); // Fin de la journée
-        return "FA" +this.ContatenationDesChiffreduJour() + this.factureRepository.countFacturesCreateToday(startOfDay, endOfDay);
+        Instant endOfDay = LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant(); // Fin de la
+                                                                                                         // journée
+
+        Long numbreoftoday = this.factureRepository.countFacturesCreateToday(startOfDay, endOfDay);
+
+        return "FA" + this.ContatenationDesChiffreduJour()
+                + (numbreoftoday <= 9 ? "0" + numbreoftoday : numbreoftoday);
     }
 
     public String GenerateBordereauNumero() {
         Instant startOfDay = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant(); // Début de la journée
-        Instant endOfDay = LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant(); // Fin de la journée
-        return "BL" + this.ContatenationDesChiffreduJour() + this.borderauRepository.countBordereauxCreateToday(startOfDay, endOfDay);
+        Instant endOfDay = LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant(); // Fin de la
+                                                                                                         // journée
+
+        Long numbreoftoday = this.borderauRepository.countBordereauxCreateToday(startOfDay, endOfDay);
+        return "BL" + this.ContatenationDesChiffreduJour()
+                + (numbreoftoday <= 9 ? "0" + numbreoftoday : numbreoftoday);
     }
 
-    private String ContatenationDesChiffreduJour(){
+    private String ContatenationDesChiffreduJour() {
         String year = String.valueOf(LocalDate.now().getYear()).substring(2); // Année (2 chiffres)
         String month = String.format("%02d", LocalDate.now().getMonthValue()); // Mois
-        String day = String.format("%02d", LocalDate.now().getDayOfMonth());  // Jour
+        String day = String.format("%02d", LocalDate.now().getDayOfMonth()); // Jour
         return year + month + day;
     }
 }
